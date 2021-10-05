4e719dcd0027103c8b240a8428761ff4f8f179eb
Ubuntu <ubuntu@ip-172-31-39-33.sa-east-1.compute.internal>
2021-10-05 21:40:06 +0000

    commented not needed if's

--- console.py
+++ console.py
@@ -21,18 +21,34 @@ class ATCAScmd(cmd.Cmd):
 	def do_quit(self, arg):
 		"""Quit command to exit the program"""
 		return True
+
 	def do_create(self, arg):
 		"creates a new class"
 		args = arg.split()
 		if len(args) == 0:
 			print("** No class selected **")
 			return False
-		if args[0] in classes:
-			if len(args) != 5:
-				print("** missing atribute**")
-				return False
-		print("llegue")
-		print(args)
+		if args[0] not in classes:
+			print("**wrong class**")
+			return False
+		if len(args) == 1:
+			print("**Created empty class, run update to set values**")
+		if len(args) == 2:
+			print("**Created with ID**")
+		if len(args) == 3:
+			print("**ID and type**")
+		if len(args) == 4:
+			print("**ID, type and registration**")
+		if len(args) == 5:
+			print("**ID, type, registration, ariline**")
+		if len(args) == 6:
+			print("**ID, type, registration, ariline, country**")
+		if len(args) == 7:
+			print("**ID, type, registration, ariline, country, ICAO**")
+	
+	def do_update(self):
+		self.update
+
 
 if __name__ == '__main__':
     ATCAScmd().cmdloop()
--- models/aircraft.py
+++ models/aircraft.py
@@ -12,9 +12,18 @@ class Aircraft:
     "Class model for all aircrafts"
 
     def __init__(self, *args, **status):
-        self.type = 1
-        self.registration = 1
-        self.airline = 1
-        self.country = 1
-        self.ICAO = 1
-        self.flightstatus  = copy.deepcopy(status)
+        self.id = 0 """query of ID in Aircrafts table"""
+        self.type = 1 """query of type in Aircrafts table"""
+        self.registration = 1 """query of regi in Aircrafts table"""
+        self.airline = 1 """query of airline in Aircrafts table"""
+        self.country = 1 """query of country in Aircrafts table"""
+        self.ICAO = 1 """query of ICAO in Aircrafts table"""
+        self.flightstatus  = {} """copy.deepcopy(status)""" """data base query"""
+
+    def __str__(self):
+        """String representation of the BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
+    def update(self):
+        """updates information of an aircraft"""
+        self.country = 2 """query of country in Aircrafts table"""
