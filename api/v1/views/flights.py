87fccf5b9aaf5d15585f846d053f951ced9f268f
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-10-20 23:05:44 +0000

    updated funtions documentation
    Co-authored-by: pichu185 <pichu185@users.noreply.github.com>

--- api/v1/views/flights.py
+++ api/v1/views/flights.py
@@ -1,47 +1,45 @@
 #!/usr/bin/python3
-"""view for 'Flights'"""
+"""
+This module contains all the endpoints of the RESTFUL API V1 of ATCAS web application,
+which the web app frontend uses to load the processed data in backend
+"""
 
-import cmd
-import json
-import models
-from models.aircraft import Aircraft
-from models.airport import Airport
-import math
 from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
-from models.aircraft import Aircraft
 from models import storage
+from models.airport import Airport
+from models.aircraft import Aircraft
 
 
 @app_views.route('/airport', methods=['GET'], strict_slashes=False)
 def create_airport():
-    """ #todo """
+    """returns information about the current working airport"""
     airport = Airport("MVD").to_dict()
     airport = Aircraft.plane_list
     return jsonify(airport)
 
+
 @app_views.route('/flights', methods=['GET'], strict_slashes=False)
 def flights():
-    """ prints all flight information based on ID's given"""
+    """returns a json with information about all flights in the airspace"""
     ret = []
     for plane in Airport.airplane_list:
         if plane not in Airport.mapped_planes:
             avion = Aircraft(str(plane))
             Airport.mapped_planes.append(plane)
-            #avion.create_estimated_flightpath()
+            avion.create_estimated_flightpath()
     for avion in Aircraft.plane_list:
         avion.update()
-        while (len(avion.estimated_flightpath) > 1):
-            avion.estimated_flightpath.remove(avion.estimated_flightpath[0])
-    #Aircraft.all_collision(Aircraft.plane_list)
-    #Aircraft.all_collision(Aircraft.plane_list)
+        while (len(avion.estimated_flightpath) > 2):
+            avion.estimated_flightpath.remove(avion.estimated_flightpath[1])
     for avv in Aircraft.plane_list:
         ret.append(avv.to_dict())
     return jsonify(ret)
 
+
 @app_views.route('/collitions', methods=['GET'], strict_slashes=False)
 def collitions():
-    """ prints all flight information based on ID's given"""
+    """returns a json with data about all flights involved in a possible collision"""
     ret = []
     Aircraft.all_collision(Aircraft.plane_list)
     allcollision_list = Airport.map_collisions
@@ -52,7 +50,7 @@ def collitions():
 
 @app_views.route('/new_route', methods=['GET'], strict_slashes=False)
 def new_route():
-    """creates a plane based in its id, if id is none, create all planes"""
+    """returns a suggested route for a flight involved in a possible collision"""
     ret = []
     for plane in Aircraft.plane_list:
         if len(plane.collision_l) != 0:
