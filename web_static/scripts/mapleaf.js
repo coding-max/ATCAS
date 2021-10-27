cb4a31c79f24bc8d90f18f03382837dcfa54a9cf
toshi-uy <s_borgia@hotmail.com>
2021-10-27 16:54:47 -0300

    fixed routes not erasing, map refreshing correctly and added Tooltip and flights on list

--- web_static/scripts/mapleaf.js
+++ web_static/scripts/mapleaf.js
@@ -5,6 +5,7 @@ const airportId = document.getElementById('center-airport');
 const center = document.getElementById('center-map');
 const collision = document.getElementById('collision');
 const totalFlights = document.getElementById('total-flights');
+const flights = document.getElementById('flights');
 
 
 // map constructor
@@ -25,14 +26,14 @@ const airplane = L.icon({
   iconSize: [30, 30], // size of the icon
   iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
   popupAnchor: [-2, -10], // point from which the popup should open relative to the iconAnchor
-  iconId: "airplane"
+  iconId: 'airplane'
 });
 const airplaneSel = L.icon({
   iconUrl: './images/airplane_sel.png',
   iconSize: [30, 30], // size of the icon
   iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
   popupAnchor: [-2, -10], // point from which the popup should open relative to the iconAnchor
-  iconId: "airplane_selected"
+  iconId: 'airplane_selected'
 });
 const airplaneRed = L.icon({
   iconUrl: './images/airplane_red.png',
@@ -40,7 +41,7 @@ const airplaneRed = L.icon({
   iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
   popupAnchor: [-2, -10] // point from which the popup should open relative to the iconAnchor
 });
-L.marker([-34.833, -56.030]).addTo(map).bindTooltip("Carrasco International Airport (SUMU)");
+L.marker([-34.833, -56.030]).addTo(map).bindTooltip('Carrasco International Airport (SUMU)');
 
 const markers_dict_selected = {};
 const route = {};
@@ -48,24 +49,25 @@ let markers = new Array();
 let polylines = new Array();
 function getPlanes() {
   const json = $.getJSON('flights.json', function (index) {
-    console.log(polylines);
     if (polylines.length === 0){
       routeDel();
     }
     totalFlights.innerHTML = Object.keys(index).length;
     const markers_dict = {};
+    flights.innerHTML = '';
     for (let i = 0; i < Object.keys(index).length; i++) {
+      flights.innerHTML += '<li class="text-center my-2 rounded flights"> <b>' + index[i].FlightID + '</b></br>'+ index[i].departure_airport + ' <i class="fas fa-arrow-right me-2"></i>' + index[i].arrival_airport + '</li>';
       if (!markers_dict_selected[index[i].FlightID]) {
         markers_dict[index[i].FlightID] = index[i];
-        markers_dict[index[i].FlightID].status = "not selected";
+        markers_dict[index[i].FlightID].status = 'not selected';
         markers_dict[index[i].FlightID].marker = L.marker([index[i].path[0].latitude, index[i].path[0].longitude], { icon: airplane, rotationAngle: index[i].path[0].truck });
-        markers_dict[index[i].FlightID].marker.addTo(map).bindTooltip("ICAO: " + index[i].ICAO).bindPopup("HOLA");
+        markers_dict[index[i].FlightID].marker.addTo(map).bindTooltip('FlightID: ' + index[i].FlightID).bindPopup('HOLA');
         markers.push(markers_dict[index[i].FlightID].marker);
       } else {
         markers_dict[index[i].FlightID] = index[i];
-        markers_dict[index[i].FlightID].status = "selected";
+        markers_dict[index[i].FlightID].status = 'selected';
         markers_dict[index[i].FlightID].marker = L.marker([index[i].path[0].latitude, index[i].path[0].longitude], { icon: airplaneSel, rotationAngle: index[i].path[0].truck });
-        markers_dict[index[i].FlightID].marker.addTo(map).bindTooltip("ICAO: " + index[i].ICAO).bindPopup("HOLA").openPopup();
+        markers_dict[index[i].FlightID].marker.addTo(map).bindTooltip('FlightID: ' + index[i].FlightID).bindPopup('HOLA').openPopup();
         markers.push(markers_dict[index[i].FlightID].marker);
         map.removeLayer(markers_dict_selected[index[i].FlightID].route);
         markers.pop(markers_dict_selected[index[i].FlightID].marker);
@@ -74,22 +76,23 @@ function getPlanes() {
         polylines.push(markers_dict[index[i].FlightID].route);
         polylines.push(markers_dict_selected[index[i].FlightID].route);
       }
-      markers_dict[index[i].FlightID].marker.on("click", function onClick(e) {
+      markers_dict[index[i].FlightID].marker.on('click', function onClick(e) {
         if (markers_dict[index[i].FlightID].status == 'not selected') {
           markers_dict[index[i].FlightID].marker.setIcon(airplaneSel);
-          markers_dict[index[i].FlightID].status = "selected";
+          markers_dict[index[i].FlightID].status = 'selected';
           markers_dict_selected[index[i].FlightID] = index[i];
           markers_dict_selected[index[i].FlightID].route = L.polyline([[index[i].path[0].latitude, index[i].path[0].longitude], [index[i].estimated_flightpath[1].latitude, index[i].estimated_flightpath[1].longitude]]);
           markers_dict_selected[index[i].FlightID].route.addTo(map);
           polylines.push(markers_dict[index[i].FlightID].route);
           polylines.push(markers_dict_selected[index[i].FlightID].route);
+          collision
         } else {
           map.removeLayer(markers_dict_selected[index[i].FlightID].route);
           map.removeLayer(markers_dict[index[i].FlightID].route);
           polylines.pop(markers_dict[index[i].FlightID].route);
           polylines.pop(markers_dict_selected[index[i].FlightID].route);
           delete markers_dict_selected[index[i].FlightID];
-          markers_dict[index[i].FlightID].status = "not selected";
+          markers_dict[index[i].FlightID].status = 'not selected';
           markers_dict[index[i].FlightID].marker.setIcon(airplane).closePopup();
         }
       });
