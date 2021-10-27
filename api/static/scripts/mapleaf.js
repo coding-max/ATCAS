32c6e1359953b61636d048c5c1c9fa89efea1faf
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-10-27 18:03:35 +0000

    added api-releated files

---
+++ api/static/scripts/mapleaf.js
@@ -0,0 +1,88 @@
+// local variables
+const montevideo = [-32.50, -55.80];
+const airport = [-34.833, -56.030];
+const airportId = document.getElementById('center-airport');
+const center = document.getElementById('center-map');
+const collision =  document.getElementById('collision');
+
+// map constructor
+var map = L.map('mapid').setView(montevideo, 7);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="#">OpenStreetMap</a> contributors, Map Propietry of &copy <a href="#">ATCAS</a>',
+    maxZoom: 18,
+    opacity: 0.8,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoidG9zaGliIiwiYSI6ImNrdjJqMzh4aTBjNTYybnA2OHAzNXl4em0ifQ.SRtHX1Z6ziF7babo6wfVtA'
+}).addTo(map);
+
+// Icon Creation
+const airplane = L.icon({
+  iconUrl: './images/airplane_1.png',
+  iconSize:     [30, 30], // size of the icon
+  iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
+  popupAnchor:  [-2, -10], // point from which the popup should open relative to the iconAnchor
+  iconId: "airplane"
+});
+const airplaneSel = L.icon({
+  iconUrl: './images/airplane_sel.png',
+  iconSize:     [30, 30], // size of the icon
+  iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
+  popupAnchor:  [-2, -10], // point from which the popup should open relative to the iconAnchor
+  iconId: "airplane_selected"
+});
+const airplaneRed = L.icon({
+  iconUrl: './images/airplane_red.png',
+  iconSize:     [30, 30], // size of the icon
+  iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
+  popupAnchor:  [-2, -10] // point from which the popup should open relative to the iconAnchor
+});
+L.marker([-34.833, -56.030]).addTo(map).bindPopup("I am an invented airplane.");
+
+function getPlanes() {
+  const json = $.getJSON('./templates/flights.json', function (index) {
+    for (let i = 0; i < Object.keys(index).length; i++) {
+      const markers_dict = {};
+      if (!markers_dict[index[i].FlightID]){
+        markers_dict[index[i].FlightID] = index[i];
+        markers_dict[index[i].FlightID].status = "not selected";
+        markers_dict[index[i].FlightID].marker = L.marker([index[i].path[0].latitude, index[i].path[0].longitude], { icon: airplane, rotationAngle: index[i].path[0].truck});
+        markers_dict[index[i].FlightID].marker.addTo(map).bindPopup("I am airplane " + index[i].ICAO + " .");
+      } else {
+          markers_dict[index[i].FlightID].marker.setLatLng([index[i].path[0].latitude, index[i].path[0].longitude]);
+      }
+      let route = L.polyline([]);
+      markers_dict[index[i].FlightID].marker.on("click", function onClick(e) {
+        if(markers_dict[index[i].FlightID].status == 'not selected') {
+          markers_dict[index[i].FlightID].marker.setIcon(airplaneSel);
+          markers_dict[index[i].FlightID].status = "selected";
+          const estimated = [];
+          estimated.push([index[i].path[0].latitude, index[i].path[0].longitude]);
+          for (let j = 0; j < index[i].estimated_flightpath.length; j++) {
+            estimated.push([index[i].estimated_flightpath[j].latitude, index[i].estimated_flightpath[j].longitude]);
+          };
+          route.remove();
+          route = L.polyline(estimated);
+          route.addTo(map);
+        } else {
+          markers_dict[index[i].FlightID].status = "not selected";
+          markers_dict[index[i].FlightID].marker.setIcon(airplane);
+          route.remove();
+        }
+      });
+    }
+  });
+  console.log(markers_dict)
+}
+
+getPlanes();
+setInterval(getPlanes, 1000000);
+//   Center Airport Button
+airportId.onclick = function () {
+  map.flyTo(airport, 14);
+};
+//   Center Map Button
+center.onclick = function () {
+  map.flyTo(montevideo, 7);
+};
---
+++ api/views/flights.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""
+This module contains all the endpoints of the RESTFUL API V1 of ATCAS web application,
+which the web app frontend uses to load the processed data in backend
+"""
+
+from flask import Flask, render_template, redirect, url_for, request
+from flask import jsonify, abort, request, make_response, render_template
+from api.views import app_views
+from models import storage
+from models.engine import db_queries
+from models.airport import Airport
+from models.aircraft import Aircraft
+
+
+@app_views.route('/airport', methods=['GET'], strict_slashes=False)
+def create_airport():
+    user = db_queries.get_user("pichu", "password")
+    if user is not None:
+        """returns information about the current working airport"""
+        airport = Airport("MVD").to_dict()
+        #airport = Aircraft.plane_list
+        return jsonify(airport)
+    return jsonify("You are not logged in")
+
+
+@app_views.route('/flights', methods=['GET'], strict_slashes=False)
+def flights():
+    """returns a json with information about all flights in the airspace"""
+    ret = []
+    for plane in Airport.airplane_list:
+        if plane not in Airport.mapped_planes:
+            avion = Aircraft(str(plane))
+            Airport.mapped_planes.append(plane)
+            avion.create_estimated_flightpath()
+    for avion in Aircraft.plane_list:
+        avion.update()
+        while (len(avion.estimated_flightpath) > 2):
+            avion.estimated_flightpath.remove(avion.estimated_flightpath[1])
+    for avv in Aircraft.plane_list:
+        ret.append(avv.to_dict())
+    return jsonify(ret)
+
+
+@app_views.route('/collitions', methods=['GET'], strict_slashes=False)
+def collitions():
+    """returns a json with data about all flights involved in a possible collision"""
+    ret = []
+    Aircraft.all_collision(Aircraft.plane_list)
+    allcollision_list = Airport.map_collisions
+    for elem in allcollision_list:
+        ret.append(elem.to_dict())
+    return jsonify(ret)
+
+
+@app_views.route('/new_route', methods=['GET'], strict_slashes=False)
+def new_route():
+    """returns a suggested route for a flight involved in a possible collision"""
+    ret = []
+    for plane in Aircraft.plane_list:
+        if len(plane.collision_l) != 0:
+            plane.suggested_flightpath = plane.create_estimated_flightpath(-1000)
+            for positions in plane.suggested_flightpath:
+                ret.append(positions.__dict__())
+    return jsonify(ret)
+
+
+@app_views.route('/accept_new_path/<string:id>', methods=['POST'], strict_slashes=False)
+def accept_new_path(id=None):
+    """"accepts new route of changes"""
+    if id is None:
+        for avion in Aircraft.plane_list:
+            if len(avion.suggested_flightpath) > 0:
+                avion.switch_manifesto()
+                avion.estimated_flightpath = avion.suggested_flightpath
+                avion.suggested_flightpath = []
+    else:
+        for avion in Aircraft.plane_list:
+            if str(avion.FlightID) == str(id):
+                if len(avion.suggested_flightpath) > 0:
+                    avion.switch_manifesto()
+                    avion.estimated_flightpath = avion.suggested_flightpath
+                    avion.suggested_flightpath = []
